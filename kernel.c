021a9a239b081cdad377777d4e4498719ab1e274
Raphael Schuhmann <raphael221@outlook.de>
2026-01-02 20:36:23 +0100

    Implemented kernel main function, Implemented printMessage function to print a string directly to vga memory

--- kernel.c
+++ kernel.c
@@ -1,4 +1,9 @@
-int kmain() {
+#define VGA_MEMORY ((volatile unsigned short *)0xB8000)
 
-    return 0;
+void printMessage(const char *msg) {
+  for (int i = 0; msg[i] != 0; i++) {
+    VGA_MEMORY[i] = (0x0F << 8) | msg[i];
+  }
 }
+
+void kmain() { printMessage("Hello World"); }
